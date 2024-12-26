89ab219ec5af38b779ecb8a5ef4b80a061d42b29
Vinod <vinodmurugan12@gmail.com>
2024-12-26 20:34:53 +0530

    Thread pool example

---
+++ Theory/20.Multi threaded Program/20.2.Thread Pool/ThreadPoolExample.java
@@ -0,0 +1,46 @@
+import java.util.concurrent.*;
+
+class Task implements Runnable
+{
+    private final String taskname;
+
+    public Task(String name)
+    {
+        this.taskname=name;
+    }
+    @Override
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName()+" Executing Thread "+taskname);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println(Thread.currentThread().getName()+" completed task "+taskname);
+    }
+}
+public class ThreadPoolExample {
+    public static void main(String[] args)
+    {
+        ExecutorService executor=Executors.newFixedThreadPool(5);
+
+        for(int i=1;i<=5;i++)
+        {
+            executor.submit(new Task("Task-"+i));
+        }
+
+        executor.shutdown();
+
+        try{
+            if(!executor.awaitTermination(60, TimeUnit.SECONDS))
+            {
+                executor.shutdownNow();
+            }
+        }
+        catch(InterruptedException e)
+        {
+            executor.shutdownNow();
+        }
+    }
+}
