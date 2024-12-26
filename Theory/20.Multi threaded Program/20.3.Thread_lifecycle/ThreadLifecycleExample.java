ea5b79e993bab90bc7e9535ab22596eed9068436
Vinod <vinodmurugan12@gmail.com>
2024-12-26 23:00:15 +0530

    Thread lifecycle

--- Theory/20.Multi threaded Program/20.3.Thread_lifecycle/ThreadLifecycleExample.java
+++ Theory/20.Multi threaded Program/20.3.Thread_lifecycle/ThreadLifecycleExample.java
@@ -35,3 +35,25 @@ public class ThreadLifecycleExample {
         System.out.println("Thread state after termination: " + t1.getState());
     }
 }
+
+
+/*
+ * Output:
+ * 
+ *  Thread state after creation: NEW
+Thread state after calling start(): RUNNABLE
+Thread state during execution: TIMED_WAITING
+Thread-0 is running.
+Thread-0 is awake.
+Thread-0 has finished execution.
+Thread state after termination: TERMINATED
+
+State	Description
+New	      ->    Thread is created but not started.
+Runnable   ->	Thread is ready to run but waiting for CPU time.
+Running	    ->  Thread is executing its task.
+Blocked     ->	Thread is waiting to acquire a lock.
+Waiting     ->	Thread is waiting indefinitely for another thread's signal.
+Timed Waiting ->	Thread is waiting for a specific time (e.g., sleep, join with timeout).
+Terminated    ->	Thread has completed its task or been stopped explicitly.
+ */
